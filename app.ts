2a7bfd9d7d614d59b16635c6f14327fa5fb83770
hamza42076 <164366638+hamza42076@users.noreply.github.com>
2024-03-27 00:43:23 +0500

    This is my second project

---
+++ app.ts
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+let answers = await inquirer.prompt([{
+    name: "hamza",
+    type: "number",
+    message: "Enter your firstnumber",
+
+},
+{
+    name: "bilal",
+    type: "number",
+    message: "Enter your secondnumber",
+},
+    {
+      message: "select one of the operator", 
+      type: "list",
+      name: "operator",
+      choices: ["addition", "subtraction", "multiplication", "Division"], 
+    },
+]);
+if (answers.operator === "addition") {
+    console.log(answers.hamza + answers.bilal)
+}
+else if (answers.operator === "subtraction") {
+    console.log(answers.hamza - answers.bilal)
+}
+else if (answers.operator === "multiplication") {
+    console.log(answers.hamza * answers.bilal)
+}
+else if (answers.operator === "Division"){
+    console.log(answers.hamza / answers.bilal)
+}
+else{
+    console.log("invalid operator")
+}
+
+    
+
